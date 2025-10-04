3770c0030accc1e062d84f7e37a086fabae4b82e
LeeChangHoon <anotherhoon@hamagroups.io>
2025-10-05 02:05:33 +0900

    ✨ feat: 리프레시토큰 기능 추가

---
+++ src/auth/dto/refresh-token.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class RefreshTokenDto {
+  @ApiProperty({
+    example: 'a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6',
+    description: '리프레시 토큰',
+  })
+  @IsNotEmpty()
+  @IsString()
+  refreshToken: string;
+}
